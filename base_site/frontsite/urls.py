d3edce5b6e120c3d4a21995203cf4309e764885c
findlogan <logaster2@hotmail.com>
2021-11-28 16:38:20 -0500

    Week 5

--- base_site/frontsite/urls.py
+++ base_site/frontsite/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('beer', views.beer, name='beer'),
+    path('coffee', views.coffee, name='coffee'),
+    path('catering', views.catering, name='catering'),
 ]
--- base_site/frontsite/views.py
+++ base_site/frontsite/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def beer(request):
+    return render(request, 'beer.html')
+    
+def coffee(request):
+    return render(request, 'coffee.html')
+    
+def catering(request):
+    return render(request, 'catering.html')
